84e4f4ef9d040be15fd0a8c58253455a6c260305
Deeti <deetigupta8@gmail.com>
2023-10-27 18:33:01 +0530

    added carousel with swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Carousels from './components/Carousels/Carousels';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
 import Verticals from './components/Verticals/Verticals';
@@ -12,6 +13,7 @@ function App() {
     <Intro/>
     <WhyUs/>
     <Verticals/>
+    <Carousels/>
    </main>
    </>
   )
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import test1 from "./assets/testi1.png"
+import test2 from "./assets/testi2.png"
+import test3 from "./assets/testi3.png"
+import test4 from "./assets/testi4.png"
+
+
+export const testimonials = [
+    {
+        img: test1,
+        name: "Jane Cooper",
+        sub: "Ceo of Hunt",
+        review: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua dolor do amet sint. Velit official"
+    },
+    {
+        img: test2,
+        name: "Devon Lane",
+        sub: "Ceo of Hunt",
+        review: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua dolor do amet sint. Velit official"
+    },
+    {
+        img: test3,
+        name: "Robert Fox",
+        sub: "Ceo of Hunt",
+        review: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua dolor do amet sint. Velit official"
+    },
+    {
+        img: test4,
+        name: "Mary Jane",
+        sub: "Ceo of Hunt",
+        review: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua dolor do amet sint. Velit official"
+    },
+]
