51976a55478dbe53868870c5f8e8fce25a959f6c
Deeti <deetigupta8@gmail.com>
2023-10-28 16:02:04 +0530

    last minute css changes

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -5,7 +5,7 @@ const Subscribe = () => {
     <div className={styles.subscribe_div}>
         <div className={styles.subscribe_content}>
             <h1>Subscribe Our Newsletter</h1>
-            <form>
+            <form className={styles.form_subscribe}>
                 <input type="text" placeholder="Name"/> 
                 <input type="email" placeholder="Email"/> 
                 <button>SEND</button> 
